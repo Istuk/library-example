f3038733beb73349c573b9fa93399454825cc812
Ilija Ištuk <ilija.istuk@outlook.com>
2021-09-07 10:37:35 -0700

    Adding unique name validations

--- src/features/books/AddBook.tsx
+++ src/features/books/AddBook.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, InputLabel, makeStyles, MenuItem, Select, TextField } from '@material-ui/core';
+import { Button, FormControl, InputLabel, makeStyles, MenuItem, Select, TextField, Typography } from '@material-ui/core';
 import { useAppSelector } from 'app/hooks';
 import AppModal from 'components/AppModal';
 import { loadCountries, selectCountries } from 'features/countries/countriesSlice';
@@ -7,6 +7,7 @@ import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import BooksService from 'services/BooksService';
+import { loadBooks, selectBooksIndex } from './booksIndexSlice';
 
 const initialFormValues = {
   title: '',
@@ -33,10 +34,21 @@ async function createBook(newBook: Book): Promise<Book> {
   return await service.createBook(newBook);
 }
 
+function isTitleUnique(title: string, books: Book[]) {
+  let unique = true;
+
+  books.forEach((book) => {
+    if (book.title === title) unique = false
+  });
+
+  return unique;
+}
+
 export default function AddBook() {
   const [isOpen, setIsOpen] = useState(false);
   const [form, setForm] = useState(initialFormValues);
   const [submitted, setSubmitted] = useState(false);
+  const [titleExists, setTitleExists] = useState(false);
   const history = useHistory();
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -44,9 +56,13 @@ export default function AddBook() {
   const {
     countries
   } = useAppSelector(selectCountries);
+  const {
+    books
+  } = useAppSelector(selectBooksIndex);
 
   useEffect(() => {
     dispatch(loadCountries());
+    dispatch(loadBooks());
   }, [dispatch]);
 
   const handleOpenModal = useCallback(() => {
@@ -63,6 +79,7 @@ export default function AddBook() {
     setSubmitted(true);
 
     if (!noEmptyFields(form)) return
+    if (!isTitleUnique(form.title, books)) return
 
     createBook(form)
       .then((book) => {
@@ -71,6 +88,9 @@ export default function AddBook() {
   }
 
   const handleChange = (field: BookFormFields) => (event: any) => {
+    if (field === 'title' && !isTitleUnique(event.target.value, books))  {
+      setTitleExists(true);
+    }
     setForm({...form, [field]: event.target.value});
   }
 
@@ -88,8 +108,11 @@ export default function AddBook() {
               label="Title"
               onChange={handleChange('title')}
               value={form.title}
-              error={submitted && form.title === ''}
+              error={(submitted && form.title === '') || titleExists}
             />
+            {titleExists && <Typography color="error">
+              This title already exists
+            </Typography>}
           </div>
           <div className={classes.fieldGroup}>
             <TextField
